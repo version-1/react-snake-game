cc880c7c19092519a7eb5bd2002a14c7a40a5682
version-1 <12.05.xxxx@gmail.com>
2018-06-10 17:30:25 +0900

    スタイル修正

--- src/components/panels/Info.js
+++ src/components/panels/Info.js
@@ -2,10 +2,49 @@ import React, { Component } from 'react';
 
 class Info extends Component {
   render() {
-    const { length, status } = this.props;
+    const { length, status, interval, setSpeed } = this.props;
     return (
       <div className="info">
-        <div>Length : {length}</div>
+        <div className="form">
+          <div className="form-group">
+            <div className="lable">Length</div>
+            <div className="content">
+              <input
+                className="input"
+                value={length}
+                min={10}
+                max={10000}
+                disabled
+                type="number"
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="label">Speed</div>
+            <div className="content speed-pannel">
+              <input
+                className="input"
+                value={interval}
+                min={10}
+                max={10000}
+                onChange={e => setSpeed(e.target.value)}
+                type="number"
+                disabled={status === 'starting'}
+              />
+              <div className="speed-pannel-icon">
+                <a className="up-arrow" onClick={() => setSpeed(interval + 50)}>
+                  ▲
+                </a>
+                <a
+                  className="down-arrow"
+                  onClick={() => setSpeed(interval - 50)}
+                >
+                  ▽
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/panels/Move.js
+++ src/components/panels/Move.js
@@ -6,14 +6,28 @@ class Move extends Component {
     return (
       <div className="move">
         <div className="col">
-          <div className="btn btn-side" onClick={() => setDirection('left')}>←</div>
+          <div
+            className="btn btn-side btn-left"
+            onClick={() => setDirection('left')}
+          >
+            ←
+          </div>
         </div>
         <div className="col">
-          <div className="btn" onClick={() => setDirection('up')}>↑</div>
-          <div className="btn" onClick={() => setDirection('down')}>↓</div>
+          <div className="btn btn-up" onClick={() => setDirection('up')}>
+            ↑
+          </div>
+          <div className="btn btn-down" onClick={() => setDirection('down')}>
+            ↓
+          </div>
         </div>
         <div className="col">
-          <div className="btn btn-side" onClick={() => setDirection('right')}>→</div>
+          <div
+            className="btn btn-side btn-right"
+            onClick={() => setDirection('right')}
+          >
+            →
+          </div>
         </div>
       </div>
     );
