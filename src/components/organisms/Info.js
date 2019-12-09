a2b84abeca3c5e475dffd356c59ed42220d79d16
jiro <j.code.public@gmail.com>
2019-12-08 23:05:29 -0800

    refactor

---
+++ src/components/organisms/Info.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import {SPEED_STEP_BY} from '../../Constant';
+
+const Info = props => {
+  const {length, status, interval, setSpeed} = props;
+  return (
+    <div className="info">
+      <div className="form">
+        <div className="form-group">
+          <div className="lable">Length</div>
+          <div className="content">
+            <input
+              className="input"
+              value={length}
+              min={10}
+              max={10000}
+              disabled
+              type="number"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="label">Speed</div>
+          <div className="content speed-pannel">
+            <input
+              className="input"
+              value={interval}
+              min={10}
+              max={10000}
+              onChange={e => setSpeed(e.target.value)}
+              type="number"
+              disabled={true}
+            />
+            <div className="speed-pannel-icon">
+              <a
+                className="up-arrow"
+                onClick={() => setSpeed(interval + SPEED_STEP_BY)}>
+                ▲
+              </a>
+              <a
+                className="down-arrow"
+                onClick={() => setSpeed(interval - SPEED_STEP_BY)}>
+                ▽
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
